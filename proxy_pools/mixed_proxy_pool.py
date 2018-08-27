2e17303756a25e3c576889764c617899910cc914
RayeRen <rayeren613@gmail.com>
2018-08-27 22:53:37 +0800

    remove unstable proxy

--- proxy_pools/mixed_proxy_pool.py
+++ proxy_pools/mixed_proxy_pool.py
@@ -59,12 +59,12 @@ class MixedProxyPool(ProxyPool):
             self.log("Karmenzind timeout", "ERR")
 
         self.log("Fetching chenjiandongx")
-        try:
-            ps = requests.get("http://%s:%s/get/1000" % (self.proxy_pool_host, self.ports['chenjiandongx']),
-                              timeout=5).json()
-            new_proxies += [list(p.values())[0] for p in ps]  # https://github.com/chenjiandongx/async-proxy-pool
-        except TimeoutError:
-            self.log("chenjiandongx timeout", "ERR")
+        # try:
+            # ps = requests.get("http://%s:%s/get/1000" % (self.proxy_pool_host, self.ports['chenjiandongx']),
+            #                   timeout=5).json()
+            # new_proxies += [list(p.values())[0] for p in ps]  # https://github.com/chenjiandongx/async-proxy-pool
+        # except TimeoutError:
+        #     self.log("chenjiandongx timeout", "ERR")
 
         for p in new_proxies:
             self.add_proxy(p)
